df6d927fa59c07371d2229fb68f2074fc624f602
Eric Yan <long@ericyan.me>
2017-02-19 16:30:36 +0800

    Implement volume blocklet in Ruby DSL

---
+++ conf/i3blocks/blocklets/volume.rb
@@ -0,0 +1,34 @@
+Blocklet.new do
+  control = "Master"
+
+  def parse(data)
+    matched = data.match(/\[(\S+)\] \[(\S+)\]/)
+    return matched[1].sub(/%/, "").to_i, matched[2] == "off"
+  end
+
+  def update(volume, is_muted)
+    if is_muted
+      icon ""
+      text "Muted"
+      color :yellow
+    else
+      icon ""
+      text "#{volume}%"
+      color :normal
+    end
+  end
+
+  on :mouse do |button|
+    result = case button
+             when 1 then `amixer set #{control} toggle`
+             when 4 then `amixer set #{control} 5%+ unmute`
+             when 5 then `amixer set #{control} 5%- unmute`
+             end
+
+    volume, is_muted = parse(result)
+    update(volume, is_muted)
+  end
+
+  volume, is_muted = parse(`amixer get #{control}`)
+  update(volume, is_muted)
+end
