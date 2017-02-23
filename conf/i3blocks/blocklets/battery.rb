d759c42c7846c0bea2805359920f6036200b14f2
Eric Yan <long@ericyan.me>
2017-02-23 22:27:58 +0800

    Keep calm when no battery detected

--- conf/i3blocks/blocklets/battery.rb
+++ conf/i3blocks/blocklets/battery.rb
@@ -1,5 +1,8 @@
 Blocklet.new do
-  def parse_upower(output)
+  def upower(device)
+    output = `upower -i $(upower -e | grep #{device})`
+    return nil if output.empty?
+
     Hash[output.each_line.collect do |line|
       kv = line.split(":", 2)
       next if kv.count != 2
@@ -9,36 +12,41 @@ Blocklet.new do
     end.compact!]
   end
 
-  battery= parse_upower(`upower -i $(upower -e | grep BAT)`)
-  ac_adapter= parse_upower(`upower -i $(upower -e | grep ACAD)`)
+  battery = upower("BAT")
+  ac_adapter = upower("ACAD")
 
-  case battery["percentage"].gsub(/%/, "").to_f
-  when 95.0..100.0
-    icon ""
-  when 75.0...95.0
-    icon ""
-  when 50.0...75.0
-    icon ""
-  when 25.0...50.0
-    icon ""
-    color :yellow
-  when 0.0...25.0
+  if battery.nil?
     icon ""
-    color :red
+    text "No battery"
+  else
+    case battery["percentage"].gsub(/%/, "").to_f
+    when 95.0..100.0
+      icon ""
+    when 75.0...95.0
+      icon ""
+    when 50.0...75.0
+      icon ""
+    when 25.0...50.0
+      icon ""
+      color :yellow
+    when 0.0...25.0
+      icon ""
+      color :red
+    end
+
+    case battery["state"]
+    when "fully-charged"
+      text battery["percentage"]
+    when "charging"
+      icon ""
+      text battery["time_to_full"]
+      color :normal
+    when "discharging"
+      text battery["time_to_empty"]
+    end
   end
 
-  if ac_adapter["online"] == "yes"
+  if !ac_adapter.nil? and ac_adapter["online"] == "yes"
     icon ""
-  end
-
-  case battery["state"]
-  when "fully-charged"
-    text battery["percentage"]
-  when "charging"
-    icon ""
-    text battery["time_to_full"]
-    color :normal
-  when "discharging"
-    text battery["time_to_empty"]
   end
 end
