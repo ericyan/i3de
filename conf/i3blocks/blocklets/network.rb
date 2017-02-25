dba80f1f601873e5a0484ffe9a5bdcd0f3c29ca4
Eric Yan <long@ericyan.me>
2017-02-25 14:35:23 +0800

    Update network blocket to indicate VPN connections

--- conf/i3blocks/blocklets/network.rb
+++ conf/i3blocks/blocklets/network.rb
@@ -1,35 +1,35 @@
 Blocklet.new do
   interface = (/dev (\S+)/ =~ `ip route | grep default | head -n 1`).nil? ? "" : $~[1]
 
-  if interface.empty?
+  case
+  when interface.start_with?("wlan")
+    ssid = `/sbin/iwgetid --raw`.strip
+    quality = `grep #{interface} /proc/net/wireless | awk '{ print $3 * 100 / 70 }'`.to_i
+
+    icon ""
+    text "#{ssid} (#{quality}%)"
+    case quality
+    when 60.0...80.0
+      color :yellow
+    when 0.0...60.0
+      color :red
+    end
+  when interface.start_with?("tun", "tap")
+    icon ""
+    text "VPN (#{interface})"
+    color :green
+  when interface.empty?
     icon ""
     text "No network"
     color :red
   else
-    is_wireless = /^wlan\d+/ === interface
-    is_down = `cat /sys/class/net/#{interface}/operstate`.strip == "down"
+    icon ""
+    text interface
+  end
 
-    if is_wireless
-      ssid = `/sbin/iwgetid --raw`.strip
-      quality = `grep #{interface} /proc/net/wireless | awk '{ print $3 * 100 / 70 }'`.to_i
-
-      icon ""
-      text "#{ssid} (#{quality}%)"
-      case quality
-      when 60.0...80.0
-        color :yellow
-      when 0.0...60.0
-        color :red
-      end
-    else
-      icon ""
-      text interface
-    end
-
-    if is_down
-      icon ""
-      text "#{interface} down"
-      color :red
-    end
+  if `cat /sys/class/net/#{interface}/operstate`.strip == "down"
+    icon ""
+    text "#{interface} down"
+    color :red
   end
 end
