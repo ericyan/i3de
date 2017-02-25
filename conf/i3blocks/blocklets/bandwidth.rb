2f5fb9dec8ee3eb62e93c187c6882a3d4fa5f51d
Eric Yan <long@ericyan.me>
2017-02-25 14:18:47 +0800

    Fix bandwidth blocklet

--- conf/i3blocks/blocklets/bandwidth.rb
+++ conf/i3blocks/blocklets/bandwidth.rb
@@ -1,5 +1,5 @@
 Blocklet.new do
-  interface = `ip route | awk -v FS='(dev |  )' '/^default/ { print $2; exit }'`.strip
+  interface = (/dev (\S+)/ =~ `ip route | grep default | head -n 1`).nil? ? "" : $~[1]
   abort "No interface available" if interface.empty?
 
   stats_file = "/dev/shm/#{interface}_statistics"
@@ -16,14 +16,17 @@ Blocklet.new do
     f.close
   end
 
-  time_window = stats_current[:time] - stats_last[:time]
-  if stats_last.nil? or time_window > 60
+  # Ignore outdated data
+  stats_last = nil if !stats_last.nil? and stats_current[:time] - stats_last[:time] > 60
+
+  if stats_last.nil?
     direction = "rx"
     bandwidth_value = 0.0
     bandwidth_unit = "Kbps"
   else
     rx_bits = (stats_current[:rx_bytes] - stats_last[:rx_bytes]) * 8
     tx_bits = (stats_current[:tx_bytes] - stats_last[:tx_bytes]) * 8
+    time_window = stats_current[:time] - stats_last[:time]
 
     direction = rx_bits > tx_bits ? "rx" : "tx"
     bandwidth_value = (rx_bits > tx_bits ? rx_bits : tx_bits) / 1000.0 / time_window
