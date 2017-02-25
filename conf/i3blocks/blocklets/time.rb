dfa08d9d0dccbcece87fee7a15c4a2cf2ecb3c27
Eric Yan <long@ericyan.me>
2017-02-25 16:02:54 +0800

    Show a calendar on right click

--- conf/i3blocks/blocklets/time.rb
+++ conf/i3blocks/blocklets/time.rb
@@ -3,8 +3,11 @@ Blocklet.new do
   text `date "+%a %I:%M%P"`
 
   on :mouse do |button, x, y|
-    if button == 1
+    case button
+    when 1
       `sensible-browser https://time.is/`
+    when 3
+      `yad --calendar --mouse --posy=24 --undecorated --no-buttons --close-on-unfocus`
     end
   end
 end
