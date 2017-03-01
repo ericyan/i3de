9a1720ad81b11c59a5ff26c9f0dc138000dd4b11
Eric Yan <long@ericyan.me>
2017-03-01 22:01:04 +0800

    Tweak battery blocklet
    Detailed changes include:
    
     1. If no battery pack present, exit sliently
     2. If battery is discharging, use colour code all the time.
     3. Show the correct icon when charging.

--- conf/i3blocks/blocklets/battery.rb
+++ conf/i3blocks/blocklets/battery.rb
@@ -16,16 +16,18 @@ Blocklet.new do
   ac_adapter = upower("ACAD")
 
   if battery.nil?
-    icon ""
-    text "No battery"
+    exit 0
   else
     case battery["percentage"].gsub(/%/, "").to_f
     when 95.0..100.0
       icon ""
+      color :green
     when 75.0...95.0
       icon ""
+      color :green
     when 50.0...75.0
       icon ""
+      color :yellow
     when 25.0...50.0
       icon ""
       color :yellow
@@ -36,17 +38,15 @@ Blocklet.new do
 
     case battery["state"]
     when "fully-charged"
+      icon "" if !ac_adapter.nil? and ac_adapter["online"] == "yes"
       text battery["percentage"]
+      color :normal
     when "charging"
-      icon ""
+      icon "" if !ac_adapter.nil? and ac_adapter["online"] == "yes"
       text battery["time_to_full"]
       color :normal
     when "discharging"
       text battery["time_to_empty"]
     end
-  end
-
-  if !ac_adapter.nil? and ac_adapter["online"] == "yes"
-    icon ""
   end
 end
