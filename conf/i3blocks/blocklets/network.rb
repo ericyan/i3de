ce602951113869e8bc958fd68565aac650b85618
Eric Yan <long@ericyan.me>
2017-03-28 23:25:46 +0800

    Replace custom connman GUI with cmst
    cmst implements (nearly) all the missing features and it is actively
    maintained.

--- conf/i3blocks/blocklets/network.rb
+++ conf/i3blocks/blocklets/network.rb
@@ -35,52 +35,9 @@ Blocklet.new do
     end
   end
 
-  def get_connections
-    return `/usr/sbin/connmanctl services`.lines.map do |line|
-      id = line.split(" ").last
-      data = Hash[`/usr/sbin/connmanctl services #{id}`.lines.drop(1).map do |line|
-        line.split(" = ").map { |s| s.strip }
-      end]
-
-      icon = case
-            when ["online", "ready"].include?(data["State"])
-              "object-select"
-            when data["Type"] == "wifi"
-              if data["Security"] != "[ none ]"
-                "network-wireless-secure-signal-excellent"
-              else
-                "network-wireless-signal-excellent"
-              end
-            when data["Type"] == "ethernet"
-              "network-wired"
-            end
-
-      {
-        id: id,
-        type: data["Type"],
-        icon: icon,
-        name: data["Name"].to_s.gsub("&", "&amp;"),
-        strength: data["Strength"]
-      }
-    end
-  end
-
-  def select_connection(connections)
-    headers = [
-      "ID:HD", "Type:HD", ":IMG", "Name", "Strength:NUM"
-    ].map { |s| "--column='#{s}'" }.join(" ")
-    lines = connections.map { |h| h.values }.map { |srv| "'#{srv.join("' '")}'" }.join(" ")
-
-    return `yad --list --height=200 --width=400 --title="Connman services" \
-            #{headers} #{lines} --print-column=1 --separator=`.strip
-  end
-
   on :mouse do |button|
     if button == 1
-      id = select_connection(get_connections)
-
-      `yad --height=200 --width=400 --borders=64 --title="Service ID" --text #{id} --no-buttons \
-       --selectable-labels --text-align=center --timeout=5 --timeout-indicator=bottom`
+      `cmst --disable-tray-icon --counter-update-rate 300`
     end
   end
 
